8d5e20e103c24fec028154d3c57848bcbc7ff073
Neal Magee <nmagee@virginia.edu>
2022-09-10 17:56:39 -0400

    Testing again

--- invoke.sh
+++ invoke.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -X POST https://api.github.com/repos/cs4740/nginx-versions/dispatches \
+curl -X POST https://api.github.com/repos/uvarc/uvarc-services/dispatches \
   -H 'Accept: application/vnd.github.everest-preview+json' \
   -u nmagee:$GITHUB_PAT \
   --data '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
