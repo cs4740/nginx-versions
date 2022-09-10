3f7af561930c90e64f389e45474f941fda2c8ada
Neal Magee <nmagee@virginia.edu>
2022-09-10 19:13:54 -0400

    Get this working?

--- invoke.sh
+++ invoke.sh
@@ -4,3 +4,11 @@ curl -X POST https://api.github.com/repos/uvarc/uvarc-services/dispatches \
   -H 'Accept: application/vnd.github.everest-preview+json' \
   -u nmagee:$GITHUB_PAT2 \
   --data '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
+
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_PAT2" \
+  -d '{"ref":"topic-branch","inputs":{"name":"Mona the Octocat","home":"San Francisco, CA"}}' \
+  https://api.github.com/repos/uvarc/uavrc-services/actions/workflows/dispatch.yaml/dispatches 
