0666ea49dcf40077c99b3d5a9eb263e343eed806
Neal Magee <nmagee@virginia.edu>
2022-09-10 18:10:10 -0400

    Debugging

--- invoke.sh
+++ invoke.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -X POST https://api.github.com/repos/uvarc/uvarc-services/dispatches \
-  -H 'Accept: application/vnd.github.everest-preview+json' \
-  -u nmagee:$GITHUB_PAT \
-  --data '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
+# curl -X POST https://api.github.com/repos/uvarc/uvarc-services/dispatches \
+#   -H 'Accept: application/vnd.github.everest-preview+json' \
+#   -u nmagee:$GITHUB_PAT \
+#   --data '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
+
+curl -X POST https://api.github.com/repos/uvarc/uvarc-services/dispatches -H 'Accept: application/vnd.github.everest-preview+json' -u nmagee:$GITHUB_PAT -d '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
