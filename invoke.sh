7553e7898112a73f4722bf779c85f693eda5d3a0
Neal Magee <nmagee@virginia.edu>
2022-09-10 17:44:36 -0400

    Extending

--- invoke.sh
+++ invoke.sh
@@ -3,4 +3,4 @@
 curl -X POST https://api.github.com/repos/cs4740/nginx-versions/dispatches \
   -H 'Accept: application/vnd.github.everest-preview+json' \
   -u nmagee:$GITHUB_PAT \
-  --data '{"event_type": "$your_event", "client_payload": { "customField": "customValue" }}'
+  --data '{"event_type": "Version update", "client_payload": { "service": "nginx-versions", "version": "1.7" }}'
